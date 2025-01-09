189295bb2c3d8a7740a2f85e26df3d845aa8e56a
dhivyak-01 <dhivyasaththi@gmail.com>
2025-01-09 15:10:58 +0530

    update

--- src/app/course/course.component.ts
+++ src/app/course/course.component.ts
@@ -45,33 +45,6 @@ export class CourseComponent implements OnInit, AfterViewInit {
     }
   }
 
-  // onBookNow(course: any): void {
-  //   const userId = this.authService.getUserIdFromToken();  // Get the user ID dynamically from the token
-  //   console.log('Course:', course);
-  //   console.log('Course ID:', course._id); 
-  //   // Check if the user is logged in and has a valid user ID
-  //   if (!userId) {
-  //     console.error('User is not logged in or token is invalid');
-  //     return;
-  //   }
-  //   if (!course._id) {
-  //     console.error('Course ID is undefined:', course);
-  //     return;
-  //   }
-  //   // Add course to cart with dynamic user ID
-  //   this.cartService.addToCart({
-  //     userId: userId,  // Fetch the user ID dynamically
-  //     courseId: course._id,
-  //     title: course.title,
-  //     price: course.price,
-  //     description: course.description,
-  //     image: course.image,
-  //     duration: course.duration,
-  //     lectures: course.lectures
-  //   });
-
-  //   // console.log('Added course to cart:', course);
-  // }
 
 
   onBookNow(course: any): void {
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -75,31 +75,26 @@ modifyDescription(description: string): string {
 
 
 
-  onBookNow(course: any): void {
-    const userId = this.authService.getUserIdFromToken();  // Get the user ID dynamically from the token
-    console.log('Course:', course);
-    console.log('Course ID:', course._id); 
-    // Check if the user is logged in and has a valid user ID
-    if (!userId) {
-      console.error('User is not logged in or token is invalid');
-      return;
-    }
-    if (!course._id) {
-      console.error('Course ID is undefined:', course);
-      return;
-    }
-    // Add course to cart with dynamic user ID
-    this.cartService.addToCart({
-      userId: userId,  // Fetch the user ID dynamically
-      courseId: course._id,
-      title: course.title,
-      price: course.price,
-      description: course.description,
-      image: course.image,
-      duration: course.duration,
-      lectures: course.lectures
-    });
+onBookNow(course: any): void {
+  console.log('Course:', course);
+  console.log('Course ID:', course._id); 
 
-    // console.log('Added course to cart:', course);
+  if (!course._id) {
+    console.error('Course ID is undefined:', course);
+    return;
   }
+
+  // Add course to cart without userId
+  this.cartService.addToCart({
+    courseId: course._id,
+    title: course.title,
+    price: course.price,
+    description: course.description,
+    image: course.image,
+    duration: course.duration,
+    lectures: course.lectures
+  });
+
+  console.log('Added course to cart:', course);
+}
 }
